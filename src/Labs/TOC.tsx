a1d7c652450d1b95d66970175d6762fd3cfd45e8
sumersb <sumers.bal@gmail.com>
2024-06-09 23:15:12 -0700

    A5 lab

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -10,8 +10,10 @@ export default function TOC() {
         className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}>Lab 2</a></li>
       <li className="nav-item"><a id="wd-a3" href="#/Labs/Lab3"
         className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}>Lab 3</a></li>
-      <li className="nav-item"><a id="wd-a3" href="#/Labs/Lab4"
+      <li className="nav-item"><a id="wd-a4" href="#/Labs/Lab4"
         className={`nav-link ${pathname.includes("Lab4") ? "active" : ""}`}>Lab 4</a></li>
+      <li className="nav-item"><a id="wd-a5" href="#/Labs/Lab5"
+        className={`nav-link ${pathname.includes("Lab5") ? "active" : ""}`}>Lab 5</a></li>
       <li className="nav-item"><a id="wd-k" href="#/Kanbas" className="nav-link">Kanbas</a></li>
       <li className="nav-item"><a id="wd-github" href="https://github.com/sumersb/kanbas-react-app" target="_blank"
         className="nav-link">My GitHub</a></li>
